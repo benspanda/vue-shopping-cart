41ded5d9ac16382b7048b64e3e08c4f551325eda
benspanda <contact@benspanda.com>
2019-06-25 23:31:53 -0400

    Initial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="layout">
+    <header class="flex bg-white shadow-md p-3 z-40 relative">
+      <div class="container mx-auto flex items-center">
+        <strong>
+          <g-link to="/">{{ $static.metaData.siteName }}</g-link>
+        </strong>
+        <nav class="ml-auto flex items-center">
+          <g-link class="ml-6" to="/">Home</g-link>
+          <g-link class="ml-6" to="/about">About</g-link>
+          <g-link class="ml-6 bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded" to="/about">Cart</g-link>
+        </nav>
+      </div>
+    </header>
+    <slot/>
+  </div>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,49 @@
+<template>
+  <Layout>
+    
+    <!-- <div class="bg-white rounded-lg p-6"> -->
+      <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+      <!-- <g-image alt="Example image" src="~/favicon.png" width="135" />
+      
+      <h1>Hello, world!</h1>
+    </div> -->
+   
+    <div class="mx-auto flex flex-col items-center bg-gray-200 py-20">
+      <h1 class="text-4xl text-gray-600">Our Products</h1>
+      <p class="text-gray-500">All of the products in our portfolio</p>
+    </div>
+    <div class="mx-auto flex container">
+      <div class="sidebar">
+        <div class="sidebar-section">
+          <h3>Sort</h3>
+          <a href="">Latest</a>
+          <a href="">Popular</a>
+          <a href="">Available</a>
+          <a href="">Coming Soon</a>
+        </div>
+      </div>
+      <div class="">
+        <div class="">
+          <g-image alt="Example image" src="~/favicon.png" width="135" />
+          <div class="">
+            <h2>Title</h2>
+            <p>2019</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  }
+}
+</script>
+
+<style>
+
+</style>
